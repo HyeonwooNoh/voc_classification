c210f5772edb3eff85b8abec2239ba56f5c8518c
HyeonwooNoh(pancake) <hyeonwoonoh@gmail.com>
2017-04-07 10:49:16 +0900

    download PascalVOC files

---
+++ data/download_data.sh
@@ -0,0 +1,14 @@
+# train / val
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+tar -xvf VOCtrainval_06-Nov-2007.tar
+rm -rf VOCtrainval_06-Nov-2007.tar
+
+# test
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
+tar -xvf VOCtest_06-Nov-2007.tar
+rm -rf VOCtest_06-Nov-2007.tar
+
+# development kit
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCdevkit_08-Jun-2007.tar
+tar -xvf VOCdevkit_08-Jun-2007.tar
+rm -rf VOCdevkit_08-Jun-2007.tar
